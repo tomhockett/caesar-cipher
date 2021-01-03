8cd097c910d58dded33bd0c4a49f35c2582db48a
Tom Hockett <tom.hockett@gmail.com>
2021-01-02 19:16:59 -0600

    refine these 2 methods in one concern

--- caesar-cipher/lib/caesar_cipher.rb
+++ caesar-cipher/lib/caesar_cipher.rb
@@ -1,24 +1,20 @@
 # frozen_string_literal: false
 
 # We don't want frozen strings since the whole point is to modify strings.
-# 'false' is default but Rubocop won't shut up about it . . .
+# yes 'false' is default but i'm being explicit on purpose.
 
-# String#capital? and String# lowercase? check for int ords of specific letters.
-# String#ord_with(shift) will return an integer ordinal shifted by
-# letters only (ignore punctuation.)
+# String#is_a_letter? => true/false
+# String#ord_with(14) => integer ordinal shifted by 14
 class String
-  def capital?
-    ord.between?(65, 90)
+  def is_a_letter?
+    (ord.between?(65, 90) || ord.between?(97, 122))
   end
 
-  def lowercase?
-    ord.between?(97, 122)
-  end
-
-  def ord_with(shift)
+  def ord_with(shift = 0)
     integer_ordinal = self.ord
     base = integer_ordinal < 91 ? 65 : 97
     shifted_char = (integer_ordinal - base) + shift
+
     (shifted_char % 26) + base
   end
 end
@@ -28,7 +24,7 @@ class CaesarCipher
     ''.tap do |str|
       phrase.each_char do |char|
         # '<<' will convert integer ordinals for you, '+=' will not.
-        str << if char.lowercase? || char.capital?
+        str << if char.is_a_letter?
                  char.ord_with(shift)
                else
                  char
